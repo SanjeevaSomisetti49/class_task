e865d11ddc670d04fbae6e8ad33ad05994ed490d
sanjay <sanjeevasomisetti377@gmail.com>
2024-01-29 06:51:53 +0000

    added mosified files to branch

--- classtask/age.sh
+++ classtask/age.sh
@@ -8,7 +8,10 @@ then
 elif [ $age -ge 13 ] && [ $age -le 19 ];
 then
 	echo "teenager"
-elif [ $age -ge 20 ];
+elif [ $age -ge 20 ] && [ $age -le 60 ];
 then
 	echo "adult"
+elif [ $age -ge 60 ];
+then
+	echo "Oldage"
 fi

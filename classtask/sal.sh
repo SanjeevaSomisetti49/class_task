133b78a687d3dfe56c07b215a89a1aa26040e0f0
sanjay <sanjeevasomisetti377@gmail.com>
2024-01-29 06:45:01 +0000

    added modified files

--- classtask/sal.sh
+++ classtask/sal.sh
@@ -5,6 +5,9 @@ read sal
 if [ $sal -lt 10000 ];
 then
 	echo "no tax"
+elif [ $sal -eq 10000 ];
+then
+	echo "2% tax"
 elif [ $sal -gt 10000 ] && [ $sal -le 25000 ];
 then
 	echo "5% tax"
--- classtask/vowel.sh
+++ classtask/vowel.sh
@@ -3,11 +3,11 @@
 echo "enter vowel"
 read char
 case $char in
-	"a") echo "it is vowel";;
-	"e") echo "it is vowel";;
-	"i") echo "it is vowel";;
-	"o") echo "it is vowel";;
-	"u") echo "it is vowel";;
+	"A") echo "it is vowel";;
+	"E") echo "it is vowel";;
+	"I") echo "it is vowel";;
+	"O") echo "it is vowel";;
+	"U") echo "it is vowel";;
 	
 esac
 
